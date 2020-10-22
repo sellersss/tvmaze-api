c08056514674c453797d59bae7d79b29b26309cd
Sellers Crisp <sellershcrisp@gmail.com>
2020-10-22 16:25:37 -0500

    fixed rendering of episodes & updated comments

--- tvmaze.js
+++ tvmaze.js
@@ -1,7 +1,10 @@
+// return array of matching query string
 async function searchShows(query) {
+  // get request
   let res = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
   let showArr = [];
 
+  // ensure array includes an image –– if not default to placeholder img
   for (let showItem of res.data) {
     let showImage;
 
@@ -11,6 +14,7 @@ async function searchShows(query) {
       showImage = `${showItem.show.image.medium}`;
     }
 
+    // objects from the array
     let showObj = {
       id: `${showItem.show.id}`,
       name: `${showItem.show.name}`,
@@ -22,10 +26,7 @@ async function searchShows(query) {
   return showArr;
 }
 
-/** Populate shows list:
- *     - given list of shows, add shows to DOM
- */
-
+// render show elements to DOM
 function populateShows(shows) {
   const $showsList = $('#shows-list');
   $showsList.empty();
@@ -39,7 +40,7 @@ function populateShows(shows) {
            <div class="card-body">
              <h5 class="card-title">${show.name}</h5>
              <p class="card-text">${show.summary}</p>
-            <button data-show-id="${show.id}" id="${show.id}" class="btn btn-primary my-2">Episodes</button>
+            <button class="btn btn-primary get-episodes">Episodes</button>
            </div>
          </div>
        </div>
@@ -50,43 +51,64 @@ function populateShows(shows) {
   }
 }
 
-function removeLink() {
-  $('#a-link').remove();
-}
+// handle search form submission
+$('#search-form').on('submit', async function handleSearch(e) {
+  e.preventDefault();
 
-/** Handle search form submission:
- *    - hide episodes area
- *    - get list of matching shows and show in shows list
- */
-
-$('#search-form').on('submit', async function handleSearch(evt) {
-  evt.preventDefault();
-
+  // object from query value
   let query = $('#search-query').val();
   if (!query) return;
 
+  // hide episodes-area
   $('#episodes-area').hide();
 
   let shows = await searchShows(query);
 
+  // call to render show elements
   populateShows(shows);
 });
 
-/** Given a show ID, return list of episodes:
- *      { id, name, season, number }
- */
-
+// return a list of episodes
 async function getEpisodes(id) {
-  // TODO: get episodes from tvmaze
-  //       you can get this by making GET request to
-  //       http://api.tvmaze.com/shows/SHOW-ID-HERE/episodes
-  // TODO: return array-of-episode-info, as described in docstring above
   let res = await axios.get(`http://api.tvmaze.com/shows/${id}/episodes`);
 
-  let ep = res.data.map((eps) => ({
-    id: ep.id,
-    name: ep.name,
-    season: ep.season,
-    number: ep.number,
+  let episodes = res.data.map((episode) => ({
+    id: episode.id,
+    name: episode.name,
+    season: episode.season,
+    number: episode.number,
   }));
+  return episodes;
 }
+
+// render episode elements to DOM
+function populateEpisodes(episodes) {
+  let $episodesList = $('#episodes-list');
+  $episodesList.empty();
+
+  // append episodes list for every name/season/episode
+  for (let episode of episodes) {
+    let $item = $(
+      `
+      <li>
+        <strong>${episode.name}</strong> 
+        (S${episode.season} | E${episode.number})
+      </li>
+      `
+    );
+    $episodesList.append($item);
+  }
+  // remove only style attribute, display
+  $('#episodes-area').removeAttr('style');
+}
+
+// handle click event on episodes button
+$('#shows-list').on('click', '.get-episodes', async function handleEpisodeClick(
+  e
+) {
+  // find the nearest parent episode id
+  let showId = $(e.target).closest('.Show').data('show-id');
+  // promise if episode id is found, call to populate episodes
+  let episodes = await getEpisodes(showId);
+  populateEpisodes(episodes);
+});
